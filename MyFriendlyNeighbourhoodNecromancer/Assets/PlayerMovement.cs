a328eca20e498f67d592ad20c4ced2b6fceb7ebc
Margir <martynas.girdziuna@gmail.com>
2021-03-24 22:36:13 +0200

    Movement update

--- MyFriendlyNeighbourhoodNecromancer/Assets/PlayerMovement.cs
+++ MyFriendlyNeighbourhoodNecromancer/Assets/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
 	public float m_Sensitivity = 0.1f;
 	public float m_MaxSpeed = 1.0f;
+	public float m_Gravity = 30f;
 
 	public SteamVR_Action_Boolean m_MovesPress = null;
 	public SteamVR_Action_Vector2 m_MoveValue = null;
@@ -36,28 +37,33 @@ public class PlayerMovement : MonoBehaviour
 	private void CalculateMovement()
 	{
 		// Movement
-		Vector3 orientationEuler = new Vector3(0, transform.eulerAngles.y, 0);
-		Quaternion orientation = Quaternion.Euler(orientationEuler);
+		Quaternion orientation = CalculateOrientation();
 		Vector3 movement = Vector3.zero;
 
 		// No movement
-		if (m_MovesPress.GetStateUp(SteamVR_Input_Sources.Any))
+		if (m_MoveValue.axis.magnitude == 0)
 			m_Speed = 0;
 
-		// Button press
-		if(m_MovesPress.state)
-		{
 			// Add
-			m_Speed += m_MoveValue.axis.y * m_Sensitivity;
+			m_Speed += m_MoveValue.axis.magnitude * m_Sensitivity;
 			m_Speed = Mathf.Clamp(m_Speed, -m_MaxSpeed, m_MaxSpeed);
 
 			// Orientation
-			movement += orientation * (m_Speed * Vector3.forward) * Time.deltaTime;
+			movement += orientation * (m_Speed * Vector3.forward);
 
-		}
+		movement.y -= m_Gravity * Time.deltaTime;
 
-		m_CharacterController.Move(movement);
 		// Apply
+		m_CharacterController.Move(movement * Time.deltaTime);
+	}
+
+	private Quaternion CalculateOrientation()
+	{
+		float rotation = Mathf.Atan2(m_MoveValue.axis.x, m_MoveValue.axis.y);
+		rotation *= Mathf.Rad2Deg;
+
+		Vector3 orientationEuler = new Vector3(0, m_Head.eulerAngles.y + rotation, 0);
+		return Quaternion.Euler(orientationEuler);
 	}
 
 	private void HandleController()
