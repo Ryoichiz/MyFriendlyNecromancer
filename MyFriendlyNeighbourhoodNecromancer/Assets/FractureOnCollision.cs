dc6e1e7ac66a1e09aad8814da03137280a48ec8b
Margir <martynas.girdziuna@gmail.com>
2021-03-25 17:17:30 +0200

    Progress before baking

---
+++ MyFriendlyNeighbourhoodNecromancer/Assets/FractureOnCollision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FractureOnCollision : MonoBehaviour
+{
+	[SerializeField] GameObject ShardsObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.tag == "Shovel")
+		{
+			this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+			this.gameObject.GetComponent<BoxCollider>().enabled = false;
+			ShardsObject.SetActive(true);
+		}
+	}
+}
--- MyFriendlyNeighbourhoodNecromancer/Assets/PlayerMovement.cs
+++ MyFriendlyNeighbourhoodNecromancer/Assets/PlayerMovement.cs
@@ -6,17 +6,16 @@ using Valve.VR;
 public class PlayerMovement : MonoBehaviour
 {
 	public float m_Sensitivity = 0.1f;
-	public float m_MaxSpeed = 1.0f;
-	public float m_Gravity = 30f;
+	public float m_MaxSpeed = 10.0f;
+	public float m_Gravity = 100f;
 
-	public SteamVR_Action_Boolean m_MovesPress = null;
 	public SteamVR_Action_Vector2 m_MoveValue = null;
 
 	private float m_Speed = 0.0f;
 
 	private CharacterController m_CharacterController = null;
 	private Transform m_CameraRig = null;
-	private Transform m_Head = null;
+	[SerializeField] Transform m_Head;
 
 	private void Awake()
 	{
@@ -25,12 +24,12 @@ public class PlayerMovement : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+		HandleHeight();
 		CalculateMovement();
     }
 
@@ -66,10 +65,23 @@ public class PlayerMovement : MonoBehaviour
 		return Quaternion.Euler(orientationEuler);
 	}
 
-	private void HandleController()
+	private void HandleHeight()
 	{
+		// Get the head in local space
+		float headHeight = Mathf.Clamp(m_Head.localPosition.y, 1, 2);
+		m_CharacterController.height = headHeight;
 
+		// Cut in half
+		Vector3 newCenter = Vector3.zero;
+		newCenter.y = m_CharacterController.height / 2;
+		newCenter.y += m_CharacterController.skinWidth;
+
+		// Move capsule
+		newCenter.x = m_Head.localPosition.x;
+		newCenter.z = m_Head.localPosition.z;
+
+		// Apply
+		m_CharacterController.center = newCenter;
 	}
-
 
 }
