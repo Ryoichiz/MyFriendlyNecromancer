d22c94eb7bb8d7bc78a9ef2579c12bf412555415
Margir <martynas.girdziuna@gmail.com>
2021-03-29 16:56:16 +0300

    Npc movements

---
+++ MyFriendlyNeighbourhoodNecromancer/Assets/NPCpatrol.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCpatrol : MonoBehaviour
+{
+	// Shows if agent is going to stop on each waypoint
+	[SerializeField]
+	bool _patrolStationary;
+
+	// How long agent is supposed to wait at waypoint
+	[SerializeField]
+	float _waitingTime = 2f;
+
+	// Chance of agent changing directions
+	[SerializeField]
+	float _changingProbability = 0.1f;
+
+	// Waypoints for agent to follow
+	[SerializeField]
+	List<WayPoint> _points;
+
+	[SerializeField]
+	Animator _animate;
+
+	[SerializeField]
+	float _maxDistance = 1;
+
+	NavMeshAgent _navMeshCharacter;
+	Transform Player;
+	int _currentPoint;
+	bool _stationaryState;
+	bool _walkingState;
+	bool _patrolForwardState;
+	bool _attackingState;
+	float _Timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+		_navMeshCharacter = this.GetComponent<NavMeshAgent>();
+		_animate = this.GetComponent<Animator>();
+		Player = GameObject.Find("Player").transform;
+		if(_navMeshCharacter == null)
+		{
+			Debug.LogError("The nav mesh agent component is not attached to " + gameObject.name);
+		}
+		else
+		{
+			if(_points != null && _points.Count >= 2)
+			{
+				_currentPoint = 0;
+				SetDestination();
+			}
+			else
+			{
+				Debug.Log("Not enough of waypoints for an agent");
+			}
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		RaycastHit hit;
+		Vector3 direction = Player.position - this.gameObject.transform.position;
+		if (Physics.Raycast(transform.position, direction, out hit, Mathf.Infinity))
+		{
+			if (hit.distance < _maxDistance)
+			{
+				float angel = Vector3.Angle(transform.position, direction);
+				if (Mathf.Abs(angel) > 90)
+				{
+					Debug.DrawRay(transform.position, direction, Color.green);
+					_animate.SetBool("IsStanding", false);
+					_animate.SetBool("IsWalking", false);
+					_animate.SetBool("IsSprinting", true);
+					_attackingState = true;
+				}
+			}
+			else
+			{
+				_animate.SetBool("IsSprinting", false);
+				Debug.DrawRay(transform.position, direction, Color.red);
+			}
+		}
+		//Checking distance from waypoint
+        if(_walkingState && _navMeshCharacter.remainingDistance <= 1.0f)
+		{
+			_walkingState = false;
+
+			//Checking if agent needs to wait
+			if(_patrolStationary)
+			{
+				_animate.SetBool("IsStanding", true);
+				_stationaryState = true;
+				_Timer = 0f;
+			}
+			else
+			{
+				_animate.SetBool("IsWalking", true);
+				ChangeDestination();
+				SetDestination();
+			}
+		}
+
+		//State if we are waiting
+		if(_stationaryState)
+		{
+			_Timer += Time.deltaTime;
+			_animate.SetBool("IsWalking", false);
+			if (_Timer >= _waitingTime)
+			{
+				
+				_stationaryState = false;
+				_animate.SetBool("IsStanding", false);
+				ChangeDestination();
+				SetDestination();
+			}
+		}
+    }
+
+	private void SetDestination()
+	{
+		if(_points != null)
+		{
+			Vector3 targetVector = _points[_currentPoint].transform.position;
+			_navMeshCharacter.SetDestination(targetVector);
+			_walkingState = true;
+			_animate.SetBool("IsWalking", true);
+		}
+	}
+
+	// Changes next patrol point into a different one, making agent not always predictable
+	// allowing agent to walk forward and backward
+	private void ChangeDestination()
+	{
+		if (UnityEngine.Random.Range(0f, 1f) <= _changingProbability)
+		{
+			_patrolForwardState = !_patrolForwardState;
+		}
+
+		if (_patrolForwardState)
+		{
+			_currentPoint = (_currentPoint + 1) % _points.Count;
+		}
+		else
+		{
+			if (--_currentPoint < 0)
+			{
+				_currentPoint = _points.Count - 1;
+			}
+		}
+	}
+}
