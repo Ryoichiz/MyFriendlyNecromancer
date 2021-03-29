d38c559fb0fcc51820988f812bb88beb0ede5d01
Margir <martynas.girdziuna@gmail.com>
2021-03-29 10:38:49 +0300

    Waypoints

---
+++ MyFriendlyNeighbourhoodNecromancer/Assets/ScriptsB/WayPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour
+{
+    [SerializeField]
+    protected float DebugDrawRadius = 1.0f;
+    // Start is called before the first frame update
+    public virtual void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, DebugDrawRadius);
+    }
+}
